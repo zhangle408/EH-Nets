e34f0d4acc17b78bc3c2e321aa412f811a5eb5b4
Le Zhang <96301710+zhangle408@users.noreply.github.com>
2022-04-18 16:08:29 +0800

    Update Discrete.py

--- models/Discrete.py
+++ models/Discrete.py
@@ -10,11 +10,11 @@ from torch.autograd import Function
 class DiscreteFunction(Function):
 
     @staticmethod
-    def forward(ctx, aux_combination, balance_weight, avg_freq, n_lego):
+    def forward(ctx, aux_combination, balance_weight, avg_freq, n_sc):
         balance_weight=torch.FloatTensor([balance_weight]).to(aux_combination.device)
         avg_freq=torch.FloatTensor([avg_freq]).to(aux_combination.device)
-        n_lego=torch.LongTensor([n_lego]).to(aux_combination.device)
-        ctx.save_for_backward(aux_combination, balance_weight, avg_freq, n_lego)
+        n_sc=torch.LongTensor([n_sc]).to(aux_combination.device)
+        ctx.save_for_backward(aux_combination, balance_weight, avg_freq, n_sc)
         proxy_combination = torch.zeros(aux_combination.size()).to(aux_combination.device)
         proxy_combination.scatter_(2, aux_combination.argmax(dim = 2, keepdim = True), 1)
         proxy_combination.requires_grad = True
@@ -25,17 +25,17 @@ class DiscreteFunction(Function):
         aux_combination = ctx.saved_tensors[0]
         balance_weight = ctx.saved_tensors[1]
         avg_freq= ctx.saved_tensors[2]
-        n_lego =int(ctx.saved_tensors[3].view(-1).cpu().numpy())
+        n_sc =int(ctx.saved_tensors[3].view(-1).cpu().numpy())
         balance_weight=float(balance_weight.view(-1).cpu().numpy())
         avg_freq=float(avg_freq.view(-1).cpu().numpy())
         # balance loss
         idxs = aux_combination.argmax(dim = 2).view(-1).cpu().numpy()
         unique, count = np.unique(idxs, return_counts = True)
         unique, count = np.unique(count, return_counts = True)
-        #avg_freq = (self.fre_num * self.out_channels ) / self.n_lego
+        
         max_freq = 0
         min_freq = 100
-        for i in range(n_lego):
+        for i in range(n_sc):
             i_freq = (idxs == i).sum().item()
             max_freq = max(max_freq, i_freq)
             min_freq = min(min_freq, i_freq)
